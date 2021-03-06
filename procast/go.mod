0cc0c4c277e2cfe0ef0c707b4e576f96dd1ca1ca
bagaking <zfaustk@qq.com>
2021-03-07 00:43:24 +0800

    feat: create procast

---
+++ procast/go.mod
@@ -0,0 +1,3 @@
+module github.com/bagaking/gotools/procast
+
+go 1.15
---
+++ procast/recover.go
@@ -0,0 +1,27 @@
+package procast
+
+import "fmt"
+
+func Recover(handler func(err error), format string, args ...interface{}) {
+	str := format
+	if len(args) > 0 {
+		str = fmt.Sprintf(format, args...)
+	}
+
+	if r := recover(); r != nil {
+		if v, ok := r.(error); ok {
+			if str == "" {
+				handler(fmt.Errorf("%w", v))
+				return
+			}
+			handler(fmt.Errorf("%s, %w", str, v))
+		} else {
+			if str == "" {
+				handler(fmt.Errorf("%v", r))
+				return
+			}
+			handler(fmt.Errorf("%s, %v", str, r))
+		}
+	}
+
+}
